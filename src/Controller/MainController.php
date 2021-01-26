f5403cfbd41fc3960fd6d4d18b3a6917205e7217
FraLeclere <72281126+FraLeclere@users.noreply.github.com>
2021-01-26 14:48:39 +0100

    TP4 avec detail test

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,12 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route ("/", name="main")
+     * @Route ("/", name="main_home")
      */
 
     public function home()
     {
        return $this->render('main/home.html.twig');
+    }
+
+    /**
+     * @Route ("/aboutUs", name="main_aboutUs")
+     */
+    public function aboutUs()
+    {
+        return $this->render('aboutUs/aboutUs.html.twig');
     }
 
 }
